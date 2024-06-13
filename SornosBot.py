1dcdbb9c208db801155526d7be26551aec7fea3b
Sornodod <65455639+Sotwotior@users.noreply.github.com>
2024-06-13 13:54:02 +0300

    Create SornosBot.py

---
+++ SornosBot.py
@@ -0,0 +1,25 @@
+import requests
+
+TOKEN = 'ТОКЕН БОТА'
+CHAT_ID = '1673134064'
+
+message = "ПеКарня включилась."
+
+def send_message(token, chat_id, message):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    return response
+
+if __name__ == "__main__":
+    response = send_message(TOKEN, CHAT_ID, message)
+    if response.status_code == 200:
+        print("Сообщение успешно отправлено")
+    else:
+        print("Ошибка при отправке сообщения")
